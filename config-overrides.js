8fd1de4467f5fd9a387088179cce520cd4f90082
Tangent Lin <tangent@usa.net>
2021-01-30 10:23:23 -0500

    add override

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,14 @@
+var InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    return config;
+  const plugins = config.plugins;
+  plugins.forEach((plugin, index) => {
+    if (plugin instanceof InlineChunkHtmlPlugin) {
+      // Inline all javascript file so it produces only one index.html
+      const overridePlugin = new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/\.js/]);
+      plugins[index] = overridePlugin;
+    }
+  });
+  return config;
 }
