bec8bd09d9cedc404bda826d82df860ea67a03d8
Tangent Lin <tangent@usa.net>
2021-01-30 11:54:23 -0500

    Improve treemap example

--- src/components/TreemapDemo/TreemapDemo.tsx
+++ src/components/TreemapDemo/TreemapDemo.tsx
@@ -6,14 +6,22 @@ import { Treemap } from 'react-vis';
 // CSS
 const Container = styled('div')`
   margin: 10px;
+  font-family: sans-serif;
 `;
+
+// @see https://github.com/uber/react-vis/blob/premodern/showcase/treemap/simple-treemap.js
+// @see https://uber.github.io/react-vis/documentation/other-charts/treemap
 
 export const TreemapDemo: FunctionComponent<TreemapDemoProps> = (props) => {
   return (
     <Container>
       <Treemap
         width={500}
-        height={500}
+        height={300}
+        colorType={'literal'}
+        colorRange={['#88572C']}
+        animation={true}
+        getColor={d => d.hex}
         title={props.title}
         data={props.data} />
     </Container>
--- src/components/TreemapDemo/sampleData.ts
+++ src/components/TreemapDemo/sampleData.ts
@@ -1,4 +1,7 @@
 import { TreemapPoint } from "react-vis";
+import * as React from 'react';
+
+const commonStyle: React.CSSProperties = {border: '1px solid #000000'};
 
 /**
  * @see https://github.com/uber/react-vis/blob/master/docs/treemap.md
@@ -10,26 +13,26 @@ export const sampleData: TreemapPoint = {
     {
       title: "cluster",
       children: [
-        {title: "AgglomerativeCluster", color: "#12939A", size: 3938},
-        {title: "CommunityStructure", color: "#12939A", size: 3812},
-        {title: "HierarchicalCluster", color: "#12939A", size: 6714},
-        {title: "MergeEdge", color: "#12939A", size: 743}
+        {title: "AgglomerativeCluster", color: "#12939A", size: 3938, style: commonStyle },
+        {title: "CommunityStructure", color: "#12939A", size: 3812, style: commonStyle},
+        {title: "HierarchicalCluster", color: "#12939A", size: 6714, style: commonStyle},
+        {title: "MergeEdge", color: "#12939A", size: 743, style: commonStyle}
       ]
     },
     {
       title: "graph",
       children: [
-        {title: "BetweennessCentrality", color: "#12939A", size: 3534},
-        {title: "LinkDistance", color: "#12939A", size: 5731},
-        {title: "MaxFlowMinCut", color: "#12939A", size: 7840},
-        {title: "ShortestPaths", color: "#12939A", size: 5914},
-        {title: "SpanningTree", color: "#12939A", size: 3416}
+        {title: "BetweennessCentrality", color: "#12939A", size: 3534, style: commonStyle},
+        {title: "LinkDistance", color: "#12939A", size: 5731, style: commonStyle},
+        {title: "MaxFlowMinCut", color: "#12939A", size: 7840, style: commonStyle},
+        {title: "ShortestPaths", color: "#12939A", size: 5914, style: commonStyle},
+        {title: "SpanningTree", color: "#12939A", size: 3416, style: commonStyle}
       ]
     },
     {
       title: "optimization",
       children: [
-        {title: "AspectRatioBanker", color: "#12939A", size: 7074}
+        {title: "AspectRatioBanker", color: "#12939A", size: 7074, style: commonStyle}
       ]
     }
   ]
